cd77d10e97c98ee5e160f0fbeb084c567e0e2994
Rohan Chakraborty <Rmchakra@usc.edu>
2016-09-04 17:17:49 -0700

    q6 added

---
+++ hw1/hw1q6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <cstring> 
+#include <fstream>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+  ifstream infile (argv[1]);
+  int size;
+  infile >> size;
+  string* words = new string [size];
+  
+  for(int i = 0; i<size; i++ )
+  {
+
+  	infile >> words[i];
+  }
+for(int i = size-1; i>-1;i--)
+{
+	cout << words[i]<<endl;
+}
+  
+  return 0;
+}
