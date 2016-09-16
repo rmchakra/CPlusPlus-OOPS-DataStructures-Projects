beba674d3db5d0cd737601a79aa7d75c0db3a1f3
Rohan Chakraborty <Rmchakra@usc.edu>
2016-09-16 16:15:24 -0700

    final split successful commit, previous was not

--- hw2/split.cpp
+++ hw2/split.cpp
@@ -18,15 +18,6 @@ struct Node {
     }
 };
 
-//helper to solve everyhting and then just set in = NULL
-/*
-if(in->next == NULL)
-	{
-		in = NULL;
-		return;
-   //base case is the end of the list
-	}
-*/
 	void helper (Node*& in, Node*& odds, Node*& evens);
 	void append(Node*& list, Node*& value);
 	void print(Node*& list);
@@ -43,29 +34,16 @@ void helper (Node*& in, Node*& odds, Node*& evens)
 	{
 		return;
 	}
-
 	Node* next = in->next;
-
 		if(in->value % 2 ==0)
 		{//if even
 			append(evens, in);
 		}
-
 		else
 		{//if odd
 			append(odds, in);
-		}	
-		//if(in->next->next == NULL)
-		//{
-		//	in->next = NULL;
-		//}
-		//return;
-	//if(next != NULL)
-	//{//recurse through to the end of the list 
+		}		
 		helper(next, odds, evens);
-
-	//}
-
 }
 
 
@@ -85,52 +63,4 @@ void append(Node*& list, Node*& value)
 		return;
 	}
 	append(list->next, value);
-}
-
-void print(Node*& list)
-{
-	if(list==NULL)
-	{
-		cout << "List is NULL"<< endl;
-		return;
-	}
-	//if(list->next == NULL)
-	//{
-		//cout << list->value <<endl;
-	//	return;
-	//}
-	cout << list->value<<endl;
-	print(list->next);
-}
-
-int main()
-{
-	Node n1, n2, n3;
-	n1.value = 1;
-	n2.value = 2;
-	n3.value = 3;
-	n1.next = &n2;
-	n2.next = &n3;
-	n3.next = NULL;
-	Node* list_head = &n1;
-	Node* odds_head = NULL;
-	Node* evens_head = NULL;
-
-	//to check append
-	Node n4;
-	n4.value = 56;
-	Node* n4_ptr = &n4;
-
-	append(list_head,n4_ptr);
-	print(list_head);
-
-	split (list_head, odds_head, evens_head);
-	//cout << "print n1" << endl;
-	//print(list_head);
-	cout << "print odds" << endl;
-	print(odds_head);
-	cout << "print evens" << endl;
-	print(evens_head);
-	
-	return 0;
 }
